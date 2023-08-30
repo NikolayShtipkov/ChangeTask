15cdad4a26cf8c8d7a51f9d4c7fcd82abd0e03fd
Nikolay Shtipkov <nikolayshtipkov@gmail.com>
2023-08-30 10:50:43 +0300

    add source code

---
+++ ChangeTask/ChangeTasks/ChangeTasks/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace ChangeTasks
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = GetFileContents();
+            var coins = GetCoins(text);
+
+            Console.WriteLine("coins/banknotes - " + string.Join(", ", coins) + "\n");
+
+            Dictionary<int, int> change = new Dictionary<int, int>();
+
+            int invalidInputCount = 0;
+
+            while (coins != null) 
+            {
+                Console.Write("Write balance here: ");
+                var input = Console.ReadLine();
+                Console.Write("\n");
+
+                int balance = 0;
+
+                //Check if it is a valid balance
+                bool isNumber = int.TryParse(input, out balance);
+                if (!isNumber)
+                {
+                    invalidInputCount++;
+
+                    Console.WriteLine("Not a valid number input.");
+                    Console.WriteLine("If empty/invalid input is entered twice the program will close.\n");
+
+                    // If enter is pressed twice in a row without a valid input it will exit the program
+                    if (invalidInputCount >= 2)
+                    {
+                        Environment.Exit(1);
+                    }
+
+                    continue;
+                } 
+                else
+                {
+                    invalidInputCount = 0;
+                }
+
+                //Calculate the balance
+                for (int i = 0; i < coins.Count; i++)
+                {
+                    int coinCount = balance / coins[i];
+                    balance %= coins[i];
+                    if (coinCount > 0)
+                    {
+                        change.Add(coins[i], coinCount);
+                    }
+                }
+
+                PrintResult(change);
+                change.Clear();
+            }
+        }
+
+        static string GetFileContents()
+        {
+            string text = "";
+
+            try
+            {
+                //Adjust path for file that needs to be read.
+                using (var sr = new StreamReader("C:\\Projects\\ChangeTask\\Coins.txt"))
+                {
+                    text = sr.ReadToEnd();
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            return text;
+        }
+
+        static List<int> GetCoins(string text) 
+        {
+            List<int> coins = text
+                .Split(';')
+                .Where(x => int.TryParse(x, out _))
+                .Select(int.Parse)
+                .OrderBy(x => -x)
+                .ToList();
+
+            return coins;
+        }
+
+        static void PrintResult(Dictionary<int, int> change)
+        {
+            foreach (var item in change)
+            {
+                Console.WriteLine(item.Value.ToString() + " x " + item.Key.ToString() + " coins/currency");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ChangeTask/ChangeTasks/XmlReader/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Xml;
+
+namespace ChangeTasks
+{
+    class Program
+    {
+        static void Main()
+        {
+            string feedUrl = "https://sports.ultraplay.net/sportsxml?clientKey=9C5E796D-4D54-42FD-A535-D7E77906541A&sportId=2357&days=7";
+
+            WebRequest request = WebRequest.Create(feedUrl);
+
+            // Get the response from the WebRequest
+            using (WebResponse response = request.GetResponse())
+            {
+                // Open the response stream
+                using (Stream stream = response.GetResponseStream())
+                {
+                    // Create an XmlReader to read the XML data
+                    using (XmlReader reader = XmlReader.Create(stream))
+                    {
+                        // Read the XML data
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader.Name);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
